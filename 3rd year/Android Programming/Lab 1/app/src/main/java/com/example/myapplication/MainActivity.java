7aa8c2aafe44fcd19d3783c736ffce458fb2a9bb
Adrian Gotca <adriangotca98@gmail.com>
2020-02-23 11:02:19 +0200

    added first laboratory of Android and Computer Graphics

---
+++ 3rd year/Android Programming/Lab 1/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void displayName(View view){
+        EditText editText= (EditText)findViewById(R.id.editText);
+        TextView textView = (TextView)findViewById(R.id.textView);
+        textView.setText(editText.getText());
+    }
+}
