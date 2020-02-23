d30949348a949a94da8b68c21bf1e435258b57a1
Adrian Gotca <adriangotca98@gmail.com>
2020-02-23 11:01:17 +0200

    made some adjustments to T3 of Genetic Algorithms

--- 2nd year/Genetic Algorithms/T3/T3/Source.cpp
+++ 2nd year/Genetic Algorithms/T3/T3/Source.cpp
@@ -12,68 +12,83 @@
 
 using namespace std;
 
-//ifstream adi("date.in");
-ofstream isti("1.out");
-vector <vector <int> > pop;
-vector <int> bestRoadSoFar;
+ofstream georgiana("1.out");
+vector <vector <int> > population;
+vector <int> bestPathSoFar;
 vector <double> fitness, values;
-int nodes, node[NMAX], x[NMAX], y[NMAX], mat[NMAX][NMAX];
-int bestSoFar=INT_MAX;
+int nodeCount, node[NMAX], x[NMAX], y[NMAX];
+double adjacencyMatrix[NMAX][NMAX], bestSoFar = INT_MAX;
 void read();
 void eval();
 double f(vector <int>);
 void euristica();
 void initializare();
 void genetic();
-int dist(int, int, int, int);
 void crossover();
 void crossover(int, int);
 void mutation();
 void select();
 int main() {
-	read();
 	srand(time(NULL));
+	read();
 	genetic();
+	//euristica();
 	return 0;
 }
 
 void euristica() {
 	cout << "Euristica: ";
-	vector <int> order;
-	int Best = INT_MAX;
-	for (int i = 1; i <= nodes; i++) {
+	vector <int> order, bestRoad;
+	int viz[NMAX];
+	double best = INT_MAX;
+	for (int i = 1; i <= nodeCount; i++) {
+		order.resize(0);
 		order.push_back(i);
-	}
-	do {
-		int cost = 0;
+		memset(viz, 0, sizeof(viz));
+		viz[i] = 1;
+		int node = 1;
+		while (order.size() < nodeCount) {
+			int ok = 0, chosen = 0;
+			double minim = 0;
+			for (int j = 1; j <= nodeCount; j++) {
+				if (!viz[j] && (ok == 0 || adjacencyMatrix[node][j] < minim)) {
+					minim = adjacencyMatrix[node][j], chosen = j, ok = 1;
+				}
+			}
+			order.push_back(chosen);
+			viz[chosen] = 1;
+			node = chosen;
+		}
+		double cost = 0;
 		for (int i = 0; i < order.size() - 1; i++) {
-			cost += mat[order[i]][order[i + 1]];
+			cost += adjacencyMatrix[order[i]][order[i + 1]];
 		}
-		cost += mat[order[order.size() - 1]][order[0]];
-		if (cost < Best) {
-			Best = cost;
+		cost += adjacencyMatrix[order[order.size() - 1]][order[0]];
+		if (cost < best) {
+			best = cost;
+			bestRoad = order;
 		}
-	} while (next_permutation(order.begin(), order.end()));
-	cout << Best << '\n';
+	}
+	cout << "Road: ";
+	for (auto x : bestRoad) {
+		cout << x << ' ';
+	}
+	cout << '\n'<<"Cost: "<<best<< '\n';
 }
 
 void read() {
-	ifstream adi("date.in");
-	adi >> nodes;
-	for (int i = 1; i <= nodes; i++) {
-		adi >> node[i] >> x[i] >> y[i];
+	ifstream sabina("date.in");
+	sabina >> nodeCount;
+	for (int i = 1; i <= nodeCount; i++) {
+		sabina >> node[i] >> x[i] >> y[i];
 	}
-	for (int i = 1; i <= nodes; i++) {
-		for (int j = i + 1; j <= nodes; j++) {
-			mat[node[i]][node[j]] = mat[node[j]][node[i]] = dist(x[i], y[i], x[j], y[j]);
+	for (int i = 1; i <= nodeCount; i++) {
+		for (int j = i + 1; j <= nodeCount; j++) {
+			int dx = x[i] - x[j];
+			int dy = y[i] - y[j];
+			adjacencyMatrix[node[i]][node[j]] = adjacencyMatrix[node[j]][node[i]] = sqrt(dx * dx + dy * dy);
 		}
 	}
-}
-
-int dist(int x1, int y1, int x2, int y2) {
-	int dx = x1 - x2;
-	int dy = y1 - y2;
-	return int(sqrt(dx * dx + dy * dy) + 0.5);
 }
 
 void genetic() {
@@ -81,25 +96,25 @@ void genetic() {
 	eval();
 	for (int gen = 1; gen <= NR_GENERATIONS; gen++) {
 		cout << "Generatia " << gen << '\n';
-		isti << "Generatia " << gen << '\n';
+		georgiana << "Generatia " << gen << '\n';
 		vector <int> bestThis;
-		int best = INT_MAX;
+		double best = INT_MAX;
 		for (int i = 0; i < POP_SIZE; i++) {
 			if (values[i] < best) {
-				bestThis = pop[i];
+				bestThis = population[i];
 				best = values[i];
 			}
 		}
-		isti << "Best in generation " << gen << ": " << best << '\n';
-		for (int i = 0; i < nodes; i++) {
-			isti << bestThis[i] << ' ';
+		georgiana << "Best in generation " << gen << ": " << best << '\n';
+		for (int i = 0; i < nodeCount; i++) {
+			georgiana << bestThis[i] << ' ';
 		}
-		isti << '\n';
-		isti << "Best so far: " << bestSoFar << '\n';
-		for (int i = 0; i < bestRoadSoFar.size(); i++) {
-			isti << bestRoadSoFar[i] << ' ';
+		georgiana << '\n';
+		georgiana << "Best so far: " << bestSoFar << '\n';
+		for (int i = 0; i < bestPathSoFar.size(); i++) {
+			georgiana << bestPathSoFar[i] << ' ';
 		}
-		isti << bestRoadSoFar[0] << '\n';
+		georgiana << bestPathSoFar[0] << '\n';
 		select();
 		crossover();
 		mutation();
@@ -111,12 +126,12 @@ void initializare() {
 	int viz[NMAX];
 	bestSoFar = INT_MAX;
 	vector <int> chromosome;
-	pop.resize(0);
+	population.resize(0);
 	for (int i = 0; i < POP_SIZE; i++) {
 		chromosome.resize(0);
 		memset(viz, 0, sizeof(viz));
-		int left = nodes;
-		for (int i = 1; i <= nodes; i++) {
+		int left = nodeCount;
+		for (int i = 1; i <= nodeCount; i++) {
 			int p = rand() % left + 1;
 			for (int j = 0, k = 1; j <= p; k++) {
 				if (!viz[k]) j++;
@@ -128,7 +143,7 @@ void initializare() {
 			}
 			left--;
 		}
-		pop.push_back(chromosome);
+		population.push_back(chromosome);
 	}
 }
 
@@ -136,21 +151,21 @@ void eval() {
 	fitness.resize(0);
 	values.resize(0);
 	for (int i = 0; i < POP_SIZE; i++) {
-		fitness.push_back(1.0/f(pop[i]));
-		values.push_back(f(pop[i]));
-		if (f(pop[i]) < bestSoFar) {
-			bestSoFar = f(pop[i]);
-			bestRoadSoFar = pop[i];
+		fitness.push_back(1.0/f(population[i]));
+		values.push_back(f(population[i]));
+		if (f(population[i]) < bestSoFar) {
+			bestSoFar = f(population[i]);
+			bestPathSoFar = population[i];
 		}
 	}
 }
 
 double f(vector <int> chromosome) {
 	double s = 0;
-	for (int i = 0; i < nodes - 1; i++) {
-		s += mat[chromosome[i]][chromosome[i + 1]];
+	for (int i = 0; i < nodeCount - 1; i++) {
+		s += adjacencyMatrix[chromosome[i]][chromosome[i + 1]];
 	}
-	s += mat[chromosome[nodes - 1]][chromosome[0]];
+	s += adjacencyMatrix[chromosome[nodeCount - 1]][chromosome[0]];
 	return s;
 }
 
@@ -170,12 +185,10 @@ void select() {
 	for (int i = 0; i < POP_SIZE; i++) {
 		double r = 1.0*rand() / RAND_MAX;
 		int survival;
-		for (survival = 0; !(wheel[survival] <= r && r <= wheel[survival + 1]); survival++) {
-			int whatever;
-		}
-		newpop.push_back(pop[survival]);
+		for (survival = 0; !(wheel[survival] <= r && r <= wheel[survival + 1]); survival++);
+		newpop.push_back(population[survival]);
 	}
-	pop = newpop;
+	population = newpop;
 }
 
 void crossover() {
@@ -187,7 +200,7 @@ void crossover() {
 		}
 	}
 	if (forCross.size() % 2 == 1) forCross.pop_back();
-	for ( ; forCross.size();) {
+	while (forCross.size()) {
 		int j = rand() % forCross.size();
 		while (j == 0) j = rand() % forCross.size();
 		crossover(forCross[0], forCross[j]);
@@ -197,44 +210,44 @@ void crossover() {
 }
 
 void crossover(int i, int j){
-	vector <int> newpopI(nodes), newpopJ(nodes);
-	vector <int> vizI(nodes + 1), vizJ(nodes + 1);
+	vector <int> newpopI(nodeCount), newpopJ(nodeCount);
+	vector <int> vizI(nodeCount + 1), vizJ(nodeCount + 1);
 	int i1 = rand() % newpopI.size();
 	int i2 = rand() % newpopI.size();
 	if (i1 > i2) swap(i1, i2);
 	for (int k = i1; k <= i2; k++) {
-		newpopI[k] = pop[j][k]; vizI[pop[j][k]] = 1;
-		newpopJ[k] = pop[i][k]; vizJ[pop[i][k]] = 1;
+		newpopI[k] = population[j][k]; vizI[population[j][k]] = 1;
+		newpopJ[k] = population[i][k]; vizJ[population[i][k]] = 1;
 	}
 	int it = 0;
-	for (int k = 0; k < nodes; k++) {
+	for (int k = 0; k < nodeCount; k++) {
 		if (it == i1) it = i2 + 1;
-		if (!vizI[pop[i][k]]) newpopI[it++] = pop[i][k];
+		if (!vizI[population[i][k]]) newpopI[it++] = population[i][k];
 	}
 	it = 0;
-	for (int k = 0; k < nodes; k++) {
+	for (int k = 0; k < nodeCount; k++) {
 		if (it == i1) it = i2 + 1;
-		if (!vizJ[pop[j][k]]) newpopJ[it++] = pop[j][k];
+		if (!vizJ[population[j][k]]) newpopJ[it++] = population[j][k];
 	}
-	pop[i] = newpopI;
-	pop[j] = newpopJ;
+	population[i] = newpopI;
+	population[j] = newpopJ;
 
 }
 
 void mutation() {
 	for (int i = 0; i < POP_SIZE; i++) {
 		vector <int> genes;
-		for (int j = 0; j < nodes; j++) {
+		for (int j = 0; j < nodeCount; j++) {
 			double p = 1.0 * rand() / RAND_MAX;
 			if (p < MUTATION_PROB) {
 				genes.push_back(j);
 			}
 		}
 		if (genes.size() % 2 == 1) genes.pop_back();
-		for (; genes.size();) {
-			int b = rand()%genes.size();
+		while (genes.size()) {
+			int b = rand() % genes.size();
 			while (b == 0) b = rand() % genes.size();
-			swap(pop[i][genes[0]], pop[i][genes[b]]);
+			swap(population[i][genes[0]], population[i][genes[b]]);
 			genes.erase(genes.begin() + b);
 			genes.erase(genes.begin());
 		}
